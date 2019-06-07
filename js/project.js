3f02517da25c9ed92b39e28a1d1910f310374d37
Gregory Giordano <greggior@Gregorys-MBP.jetpack>
2019-06-07 14:05:27 -0600

    basic front-end

---
+++ js/project.js
@@ -0,0 +1,5 @@
+function getFortune(){
+	document.getElementById("wholeCookie").style.display = "none";
+	document.getElementById("cookie").innerHTML = "<img src='images/Fortune_cookie_broken.jpg'/>";
+	document.getElementById("fortune").innerHTML = "The Class will get an A!";
+}
